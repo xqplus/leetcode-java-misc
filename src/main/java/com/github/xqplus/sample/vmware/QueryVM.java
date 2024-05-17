2516221ff9f938a5811bc75c0ed106dec8a65226
chenqiang <chenq>
2024-05-17 18:48:28 +0800

    VMware查询虚拟机磁盘变化量示例

--- src/main/java/com/github/xqplus/sample/vmware/QueryVM.java
+++ src/main/java/com/github/xqplus/sample/vmware/QueryVM.java
@@ -2,42 +2,46 @@ package com.github.xqplus.sample.vmware;
 
 import com.vmware.vim25.DiskChangeExtent;
 import com.vmware.vim25.DiskChangeInfo;
-import com.vmware.vim25.mo.InventoryNavigator;
-import com.vmware.vim25.mo.ServiceInstance;
-import com.vmware.vim25.mo.VirtualMachine;
+import com.vmware.vim25.mo.*;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.rmi.RemoteException;
 
+/**
+ * 查询虚拟机相关信息
+ *
+ * @author chenq
+ */
 public class QueryVM {
 
     public static void main(String[] args) throws MalformedURLException, RemoteException, InterruptedException {
-        ServiceInstance serviceInstance = new ServiceInstance(new URL("https://192.168.8.12/sdk"),
-                "administrator@vsphere.local", "Dsg_123456!", true);
-//        ServiceInstance serviceInstance = new ServiceInstance(new URL("https://192.168.7.2/sdk"),
-//                "root", "dsgcd_201231", true);
-//        ServiceInstance serviceInstance = new ServiceInstance(new URL("https://192.168.6.155/sdk"),
-//                "root", "dsgcd_211208", true);
+//        ServiceInstance serviceInstance = new ServiceInstance(new URL("https://192.168.8.12/sdk"),
+//                "administrator@vsphere.local", "Dsg_123456!", true);
 //        ServiceInstance serviceInstance = new ServiceInstance(new URL("https://192.168.6.2/sdk"),
 //                "root", "dsgcd_211208", true);
+        ServiceInstance serviceInstance = new ServiceInstance(new URL("https://192.168.6.178/sdk"),
+                "root", "dsgcd_211208", true);
 
-        VirtualMachine virtualMachine = (VirtualMachine) new InventoryNavigator(serviceInstance.getRootFolder())
-                .searchManagedEntity("VirtualMachine", "centos7");
-
-//        Datastore datastore = (Datastore) new InventoryNavigator(serviceInstance.getRootFolder())
-//                .searchManagedEntity("Datastore", "ssd2");
-//
-//        VirtualMachineRelocateSpec vmrs = new VirtualMachineRelocateSpec();
-//        vmrs.datastore = datastore.getMOR();
-//        Task task = virtualMachine.relocateVM_Task(vmrs);
-//        System.out.println(task.getTaskInfo().error.localizedMessage);
-
-        DiskChangeInfo diskChangeInfo = virtualMachine.queryChangedDiskAreas(virtualMachine.getCurrentSnapShot(), 2000, 0, "*");
-        for (DiskChangeExtent changeExtent : diskChangeInfo.getChangedArea()) {
-            System.out.println(changeExtent.start + ", " + changeExtent.length);
-        }
+        VirtualMachine vm = queryVirtualMachine(serviceInstance.getRootFolder(), "c7");
+        String changeId = "*";
+        // 52 6f f7 d2 fd 27 ac b1-76 44 e2 ef 5f b6 1a 84/12  vinchin
+        // 52 6f f7 d2 fd 27 ac b1-76 44 e2 ef 5f b6 1a 84/16  bdmp
+        queryChangedDiskAreas(vm, vm.getCurrentSnapShot(), 2000, changeId);
 
         serviceInstance.getServerConnection().logout();
+    }
+
+    public static VirtualMachine queryVirtualMachine(Folder rootFolder, String name) throws RemoteException {
+        return (VirtualMachine) new InventoryNavigator(rootFolder).searchManagedEntity("VirtualMachine", name);
+    }
+
+    public static void queryChangedDiskAreas(VirtualMachine vm, VirtualMachineSnapshot snapshot,
+                                             int deviceKey, String changeId) throws RemoteException {
+        DiskChangeInfo diskChangeInfo = vm.queryChangedDiskAreas(snapshot, deviceKey, 0, changeId);
+        System.out.println(diskChangeInfo.startOffset + ", " + diskChangeInfo.length);
+        for (DiskChangeExtent changeExtent : diskChangeInfo.changedArea) {
+            System.out.println(changeExtent.start + ", " + changeExtent.length);
+        }
     }
 }
