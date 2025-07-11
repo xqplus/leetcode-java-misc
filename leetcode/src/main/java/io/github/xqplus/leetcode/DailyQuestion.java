7cc34d76c6df30fc5e246ad14bb9307576de00da
chenqiang <chenq>
2025-07-11 18:10:05 +0800

    2025/7/11

--- leetcode/src/main/java/io/github/xqplus/leetcode/DailyQuestion.java
+++ leetcode/src/main/java/io/github/xqplus/leetcode/DailyQuestion.java
@@ -1346,10 +1346,125 @@ public class DailyQuestion {
     }
 
     /**
+     * 3440. 重新安排会议得到最多空余时间 II
+     * 给你一个整数 eventTime 表示一个活动的总时长，这个活动开始于 t = 0 ，结束于 t = eventTime 。
+     * 同时给你两个长度为 n 的整数数组 startTime 和 endTime 。它们表示这次活动中 n 个时间 没有重叠 的会议，其中第 i 个会议的时间为 [startTime[i], endTime[i]] 。
+     * 你可以重新安排 至多 一个会议，安排的规则是将会议时间平移，且保持原来的 会议时长 ，你的目的是移动会议后 最大化 相邻两个会议之间的 最长 连续空余时间。
+     * 请你返回重新安排会议以后，可以得到的 最大 空余时间。
+     * 注意，会议 不能 安排到整个活动的时间以外，且会议之间需要保持互不重叠。
+     * 注意：重新安排会议以后，会议之间的顺序可以发生改变。
+     * 示例 1：
+     * 输入：eventTime = 5, startTime = [1,3], endTime = [2,5]
+     * 1-2 3-5 1 1 0
+     * 输出：2
+     * 解释：
+     * 将 [1, 2] 的会议安排到 [2, 3] ，得到空余时间 [0, 2] 。
+     * 示例 2：
+     * 输入：eventTime = 10, startTime = [0,7,9], endTime = [1,8,10]
+     * 0-1 7-8 9-10  0 6 1 0
+     * 输出：7
+     * 解释：
+     * 将 [0, 1] 的会议安排到 [8, 9] ，得到空余时间 [0, 7] 。
+     * 示例 3：
+     * 输入：eventTime = 10, startTime = [0,3,7,9], endTime = [1,4,8,10]
+     * 0-1 3-4 7-8 9-10  0 1* 2 1* 3 1* 1 1* 0
+     * 输出：6
+     * 解释：
+     * 将 [3, 4] 的会议安排到 [8, 9] ，得到空余时间 [1, 7] 。
+     * 示例 4：
+     * 输入：eventTime = 5, startTime = [0,1,2,3,4], endTime = [1,2,3,4,5]
+     * 输出：0
+     * 解释：
+     * 活动中的所有时间都被会议安排满了。
+     * 提示：
+     * 1 <= eventTime <= 10^9
+     * n == startTime.length == endTime.length
+     * 2 <= n <= 10^5
+     * 0 <= startTime[i] < endTime[i] <= eventTime
+     * endTime[i] <= startTime[i + 1] 其中 i 在范围 [0, n - 2] 之间。
+     */
+    public int maxFreeTime(int eventTime, int[] startTime, int[] endTime) {
+        // 0-1 3-4 7-8 9-10
+        // 空闲： 0 2 3 1 0
+        // 会议：  1 1 1 1
+        // 枚举会议，看能否移动到空闲的时间，取最大值
+        int n = startTime.length;
+        boolean[] canMove = new boolean[n];
+        int max = startTime[0];
+        for (int i = 1; i < n; i++) {
+            if (endTime[i] - startTime[i] <= max) {
+                canMove[i] = true;
+            }
+            max = Math.max(max, startTime[i] - endTime[i - 1]);
+        }
+        max = eventTime - endTime[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            if (endTime[i] - startTime[i] <= max) {
+                canMove[i] = true;
+            }
+            max = Math.max(max, startTime[i + 1] - endTime[i]);
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int left = i == 0 ? 0 : endTime[i - 1];
+            int right = i == n - 1 ? eventTime : startTime[i + 1];
+            if (canMove[i]) {
+                ans = Math.max(ans, right - left);
+            } else {
+                ans = Math.max(ans, right - left - (endTime[i] - startTime[i]));
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 3169. 无需开会的工作日
+     * 给你一个正整数 days，表示员工可工作的总天数（从第 1 天开始）。另给你一个二维数组 meetings，长度为 n，
+     * 其中 meetings[i] = [start_i, end_i] 表示第 i 次会议的开始和结束天数（包含首尾）。
+     * 返回员工可工作且没有安排会议的天数。
+     * 注意：会议时间可能会有重叠。
+     * 示例 1：
+     * 输入：days = 10, meetings = [[5,7],[1,3],[9,10]] 4 8
+     * 输出：2
+     * 解释：
+     * 第 4 天和第 8 天没有安排会议。
+     * 示例 2：
+     * 输入：days = 5, meetings = [[2,4],[1,3]] 5
+     * 输出：1
+     * 解释：
+     * 第 5 天没有安排会议。
+     * 示例 3：
+     * 输入：days = 6, meetings = [[1,6]]
+     * 输出：0
+     * 解释：
+     * 所有工作日都安排了会议。
+     * 提示：
+     * 1 <= days <= 10^9
+     * 1 <= meetings.length <= 10^5
+     * meetings[i].length == 2
+     * 1 <= meetings[i][0] <= meetings[i][1] <= days
+     */
+    public static int countDays(int days, int[][] meetings) {
+        Arrays.sort(meetings, Comparator.comparingInt(a -> a[0]));
+        int prev = 1, ans = 0;
+        for (int[] meeting : meetings) {
+            if (meeting[0] > prev) {
+                ans += meeting[0] - prev;
+            }
+            prev = Math.max(prev, meeting[1] + 1);
+//            prev = meeting[1] + 1;
+        }
+        if (days >= prev) {
+            ans += days - prev + 1;
+        }
+        return ans;
+    }
+
+    /**
      * @param args
      */
     public static void main(String[] args) {
-        int[][] nums = {{1,2}, {1,2}, {3,3},{1,5},{1,5}};
-        System.out.println(maxEvents(nums));
+        int[][] nums = {{3,49},{23,44},{21,56},{26,55},{23,52},{2,9},{1,48},{3,31}};
+        System.out.println(countDays(57, nums));
     }
 }
